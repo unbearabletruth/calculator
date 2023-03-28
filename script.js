bcbe7b26f182a7d1d317f09805ded96332d2c53a
unbearabletruth <antaarty@gmail.com>
2023-03-28 12:22:13 +0700

    First success with adding
    Used split method to separate string into first and second numbers

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let first_n;
+let second_n;
+let operator;
+
 function add(x, y){
     return x + y;
 }
@@ -17,20 +21,42 @@ function divide(x, y){
 function help(){
     console.log("1 - add; 2 - substract; 3 - multiply, 4 - divide")
 }
-function calculate(){
+
+function calculate(first, second, op){
     help();
-    let first_n = prompt("first number:")
-    let second_n = prompt("second number:")
-    let operator = prompt("choose what to do:");
     if (operator == "1"){
-        console.log(add(first_n, second_n));
-    } else if (operator == "2"){
-        console.log(substract(first_n, second_n));
-    } else if (operator == "3"){
-        console.log(multiply(first_n, second_n));
-    } else if (operator == "4"){
-        console.log(divide(first_n, second_n));
+        return(add(first, second));
+    } else if (op == "2"){
+        console.log(substract(first, second));
+    } else if (op == "3"){
+        console.log(multiply(first, second));
+    } else if (op == "4"){
+        console.log(divide(first, second));
     }
 }
 
-//calculate();
+
+
+const numbers = document.getElementsByTagName("button");
+
+function populate(e){
+    let display = document.querySelector("#display");
+    display.textContent += e.target.textContent;
+    if (e.target.textContent === "+"){
+        operator = "1";
+        
+        console.log(first_n)
+        console.log(operator)
+        
+    }
+    [first_n, second_n] = display.textContent.split("+");
+    console.log(first_n)
+    console.log(second_n)
+    if (e.target.textContent === "="){
+        display.textContent = calculate(parseInt(first_n), parseInt(second_n), operator)
+    }
+  }   
+
+for (number of numbers){
+number.addEventListener("click", populate);
+} 
