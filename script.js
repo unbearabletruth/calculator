666727f77920e9c2e93e5aaa54570a78b59bc620
unbearabletruth <antaarty@gmail.com>
2023-03-30 12:29:31 +0700

    Doing a better version
    Have to make it wipe() if adding number to result

--- script.js
+++ script.js
@@ -1,64 +1,93 @@
-let first_n;
-let second_n;
-let operator;
-const numbers = document.getElementsByTagName("button");
-const clearing = document.getElementById("clear");
+let previous = "";
+let current = "";
+let operator = "";
+let display_value;
+
+const all_numbers = document.querySelectorAll(".numbers");
+all_numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        append_number(number.textContent);
+    });  
+});
+
+const all_operators = document.querySelectorAll(".operators");
+all_operators.forEach(element => {
+    element.addEventListener("click", () => {
+        choose_operator(element.textContent);
+    })
+});
+
+const equals = document.querySelector("#eq");
+equals.addEventListener("click", result)
+
+const clear_display = document.querySelector("#clear");
+clear_display.addEventListener("click", wipe);
+
 let display = document.querySelector("#display");
 
-function add(x, y){
-    return x + y;
-}
-
-function substract(x, y){
-    return x - y;
-}
-
-function multiply(x, y){
-    return x * y;
-}
-
-function divide(x, y){
-    return x / y;
-}
-
-function calculate(first, second, op){
-    if (op == "+"){
-        return(add(first, second));
-    } else if (op == "-"){
-        return(substract(first, second));
-    } else if (op == "*"){
-        return(multiply(first, second));
-    } else if (op == "/"){
-        return(divide(first, second));
+function calculate(){
+    previous = Number(previous);
+    current = Number(current);
+    if (operator === "+"){
+        return previous + current;
+    } else if (operator === "-"){
+        return previous - current;
+    } else if (operator === "*"){
+        return previous * current;
+    } else if (operator === "/"){
+        return previous / current;
     }
 }
 
-function populate(e){
-    display.textContent += e.target.textContent;
-    [first_n, second_n] = display.textContent.split(/[*+-/]/);
-    let contentarray = display.textContent.split("");
-    let operators = contentarray.filter(char => char === "+" || char === "-" || char === "*" || char === "/");
-    let helper = display.textContent;
-    let slicer = 2;
-    if (e.target.textContent === "="){
-        for (operator of operators){
-            display.textContent = calculate(parseInt(first_n), parseInt(second_n), operator);
-            second_n = helper.split(/[*+-/]/).slice(slicer);
-            slicer++;
-            first_n = display.textContent;
-        }
+function update_display(){
+    display.textContent = display_value;
+}
+
+function append_number(number){
+    current += number;
+    display_value = current;
+    update_display();
+}
+
+function choose_operator(op){
+    if (previous === ""){
+        previous = current;
+        operator = op;
+        display_value = "";
+        update_display();
+        current = "";
+    } else {
+        display_value = calculate();
+        console.log(display_value);
+        update_display();
+        previous = display_value;
+        operator = op;
+        current = "";
+    }   
+}
+
+function result(){
+    if (current === "" || previous === ""){
+        return
     }
+    display_value = roundnumber(calculate());
+    update_display();
 }
 
-function clear(){
-    clearing.removeEventListener("click", populate);
-    display.textContent = "";
+function roundnumber(num){
+    return Math.round(num * 100000) / 100000;
 }
 
-clearing.addEventListener("click", clear);
+function wipe(){
+    previous = "";
+    current = "";
+    operator = "";
+    display_value = "";
+    update_display();
+}
 
-for (number of numbers){
-number.addEventListener("click", populate);
-} 
+    
+
+
 
 
